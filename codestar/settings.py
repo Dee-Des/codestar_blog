1af0c9c5b60b26d70d35080ab19273912731d093
Dee-Des <desmonddee2014@gmail.com>
2025-11-13 12:20:45 +0000

    set the DEBUG constant to False and append the '.herokuapp.com' hostname to the ALLOWED_HOSTS list

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o2--8q+ocx69s+)!m7_-69!1)_#x6pk&w+aclikk2io)j-n)$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition

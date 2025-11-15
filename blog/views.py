95eb23bbb6e5eb204831d89970dc40fc89aa4384
Dee-Des <desmonddee2014@gmail.com>
2025-11-15 16:39:10 +0000

    add a filter to the queryset so can now leave some posts in Draft while finish them and they will not show on the live blog

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,26 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    # can now leave some posts in Draft while finish them and they will not show
+    #on the live blog
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
+    # to display all of the objects, or records, from the Post model.by all users
+   # queryset = Post.objects.all()
+
+    #to display only the objects, or records (blog posts here) by second user created
+    #queryset = Post.objects.filter(author=2)
+    #template_name = "post_list.html"
+
+    # another method we can use is order_by
+    # order_by allows us to specify the ordering of our records
+    # to display all of our post s ordered from teh earliest date
+    # to the most recent, we could use this:
+    # queryset = Post.objects.all().order_by("created_on")
+
+    # To reverse the order, put a minus sign in front of the field 
+    # name. 
+    # So, to order in descending order - from most recent to earliest, 
+    # which is the most sensible way of ordering blog posts, we would use:
+    # Post.objects.all().order_by("-created_on")
+    # the ordering works with filters too

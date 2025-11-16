8d92b1814361fbeed87a102c93d5b24887197f25
Dee-Des <desmonddee2014@gmail.com>
2025-11-16 17:55:28 +0000

    build a blog post view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -28,3 +28,26 @@ class PostList(generic.ListView):
     # which is the most sensible way of ordering blog posts, we would use:
     # Post.objects.all().order_by("-created_on")
     # the ordering works with filters too
+
+    def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )

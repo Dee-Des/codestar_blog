5161871ecf88b987515decfbcb5088a5d6955095
Dee-Des <desmonddee2014@gmail.com>
2025-11-18 12:49:30 +0000

    add print statements which will appear in the terminal to blog views.py file

--- blog/views.py
+++ blog/views.py
@@ -51,6 +51,7 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -61,8 +62,9 @@ def post_detail(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
-                
-    comment_form = CommentForm()
+
+    comment_form = CommentForm(data=request.POST)
+    print("About to render template")
 
     return render(
     request,

dba25026b0a72df4379e13c44d38aaf0003e7274
Dee-Des <desmonddee2014@gmail.com>
2025-11-19 16:35:22 +0000

    add tidying up to the project, including some of the required docstrings etc

--- blog/views.py
+++ blog/views.py
@@ -41,6 +41,12 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+    An instance of :form:`blog.CommentForm`.
 
     **Template:**
 
@@ -52,7 +58,7 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
-        print("Received a POST request")
+       
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -65,8 +71,7 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm(data=request.POST)
-    print("About to render template")
-
+   
     return render(
     request,
     "blog/post_detail.html",
@@ -79,7 +84,16 @@ def post_detail(request, slug):
 
 def comment_edit(request, slug, comment_id):
     """
-    view to edit comments
+    Display an individual comment for edit.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`.
     """
     if request.method == "POST":
 
@@ -102,7 +116,14 @@ def comment_edit(request, slug, comment_id):
 
 def comment_delete(request, slug, comment_id):
     """
-    view to delete comment
+    Delete an individual comment.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`
+    ``comment``
+        A single comment related to the post.
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
